a300946e2ad61c9f1e85a85073d984eb35330a16
Yolène <91735106+Yawleen@users.noreply.github.com>
2023-01-14 16:17:06 +0100

    (feat:) add home page + routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
     <div className="App">
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
