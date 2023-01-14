5aca570593b9874f74520988315f1606b54d7dee
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-01-14 15:38:44 +0100

    (refactor:) création du component Panier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Panier from './components/Panier/Panier';
+
 function App() {
   return (
     <div className="App">
+        {/* ajout du component pour pouvoir le visualiser TODO le retirer */}
+        <Panier/>
     </div>
   );
 }
